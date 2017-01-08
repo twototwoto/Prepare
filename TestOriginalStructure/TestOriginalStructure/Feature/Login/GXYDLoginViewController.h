ac62c2d6fefc78a7c88839dc08e74cb2fb8e561c
twototwoto <15076509228@163.com>
2017-01-08 11:26:01 +0800

    先简单测试一下

---
+++ TestOriginalStructure/TestOriginalStructure/Feature/Login/GXYDLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  GXYDLoginViewController.h
+//  TestOriginalStructure
+//
+//  Created by Live on 2017/1/7.
+//  Copyright © 2017年 ITCoderW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXYDLoginViewController : UIViewController
+
+@end
---
+++ TestOriginalStructure/TestOriginalStructure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestOriginalStructure
+//
+//  Created by Live on 2017/1/7.
+//  Copyright © 2017年 ITCoderW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
